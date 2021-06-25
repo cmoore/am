da5c14e98f41514e8212fdcdf215dda8ae5c0ef0
Clint Moore <cmoore@ravenpack.com>
2021-06-25 09:23:17 -0700

    honk

--- html/assets/js/app-form-elements.js
+++ html/assets/js/app-form-elements.js
@@ -2,28 +2,28 @@ var App = (function () {
   'use strict';
 
   App.formElements = function( ){
-
     //Js Code
-    $(".datetimepicker").datetimepicker({
+    $("#timestamp").datetimepicker( {
     	autoclose: true,
+      format: 'yyyy-mm-ddThh:ii:ssZ',
     	componentIcon: '.s7-date',
-    	navIcons:{
+    	navIcons: {
     		rightIcon: 's7-angle-right',
     		leftIcon: 's7-angle-left'
     	}
     });
-    
+
     //Select2
     $(".select2").select2({
       width: '100%'
     });
-    
+
     //Select2 tags
     $(".tags").select2({tags: true, width: '100%'});
 
     //Bootstrap Slider
-    $('.bslider').bootstrapSlider();
-    
+    // $('.bslider').bootstrapSlider();
+
   };
 
   return App;
--- html/assets/js/app-tables-datatables.js
+++ html/assets/js/app-tables-datatables.js
@@ -3,10 +3,9 @@ var App = (function () {
 
   App.dataTables = function( ){
 
-    //We use this to apply style to certain elements
+      //We use this to apply style to certain elements
     $.extend( true, $.fn.dataTable.defaults, {
-      dom:
-        "<'row am-datatable-header'<'col-sm-6'l><'col-sm-6'f>>" +
+      dom: "<'row am-datatable-header'<'col-sm-6'l><'col-sm-6'f>>" +
         "<'row am-datatable-body'<'col-sm-12'tr>>" +
         "<'row am-datatable-footer'<'col-sm-5'i><'col-sm-7'p>>"
     } );
@@ -16,8 +15,8 @@ var App = (function () {
     //Remove search & paging dropdown
     $("#table2").dataTable({
       pageLength: 6,
-      dom:  "<'row am-datatable-body'<'col-sm-12'tr>>" +
-            "<'row am-datatable-footer'<'col-sm-5'i><'col-sm-7'p>>"
+      dom: "<'row am-datatable-body'<'col-sm-12'tr>>" +
+        "<'row am-datatable-footer'<'col-sm-5'i><'col-sm-7'p>>"
     });
 
     //Enable toolbar button functions
@@ -30,6 +29,24 @@ var App = (function () {
             "<'row am-datatable-body'<'col-sm-12'tr>>" +
             "<'row am-datatable-footer'<'col-sm-5'i><'col-sm-7'p>>"
     });
+
+      //Enable toolbar button functions
+      $("#displaytable").dataTable({
+        buttons: [ 'copy', 'excel', 'pdf', 'print' ],
+        serverSide: true,
+        ajax: '/d',
+        searching: true,
+        // columns: [ {data: 0},
+        //            {data: 1},
+        //            {data: 2},
+        //            {data: 3},
+        //            {data: 4}
+        //          ],
+        "lengthMenu": [[6, 10, 25, 50, -1], [6, 10, 25, 50, "All"]],
+        dom:"<'row am-datatable-header'<'col-sm-6'l><'col-sm-6'f>>" +
+          "<'row am-datatable-body'<'col-sm-12'tr>>" +
+          "<'row am-datatable-footer'<'col-sm-5'i><'col-sm-7'p>>"
+      });
 
   };
 
